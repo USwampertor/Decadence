bbbdca578ae21a428c09df2b7280ad84dc8616a4
Marco <idv16c.mmillan@uartesdigitales.edu.mx>
2018-07-29 13:40:30 -0500

    +Added character and grab

--- Decadence Playground/Assets/Scripts/Grab.cs
+++ Decadence Playground/Assets/Scripts/Grab.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Grab : MonoBehaviour {
 
     public bool isHolding;
+    private Quaternion beforeGrabRotation;
     GameObject carryingObject = null;
     IneractableObject objectScript;
     // Use this for initialization
@@ -17,59 +18,69 @@ public class Grab : MonoBehaviour {
     void Update()
     {
         CheckifHolding();
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            if (!isHolding) PickUp();
-            else DropIt();
-        }
+        ///TENDRIAS QUE HACER EN UNA MAQUINA DE ESTADOS COMO PUEDE MOVERSE
+        ///PERO COMO SE QUE AHORITA NO TENEMOS TIEMPO PARA ESO
+        ///HAS UN UPDATE PARA EL MOVIMIENTO DE CUANDO ESTAS SIN AGARRAR LAS COSAS Y EN CHECKIFHOLDING PONDRE EL UPDATE
+        ///PARA CUANDO ESTES AGARRANDO ALGO
+        if (Input.GetKeyDown(KeyCode.G))  PickUp();
+        if (Input.GetKeyUp(KeyCode.G)) DropIt();
     }
     void CheckifHolding()
     {
         if (isHolding && carryingObject)
         {
-            carryingObject.transform.localPosition = transform.forward; //new Vector3(transform.position.x, transform.position.y, transform.position.z + 2.0f);
-            //carryingObject.transform.rotation = Quaternion.LookRotation(carryingObject.transform.forward, transform.up);
+            carryingObject.transform.localPosition = transform.forward;
+            transform.rotation = beforeGrabRotation;
+            // _InputDir = joystick.GetInputDirection();
+            // Joy Stick input inputX = _InputDir.x; inputY = _InputDir.y; if (Mathf.Abs (inputX) > Mathf.Abs (inputY)) { inputY = 0; } else { inputX = 0; }
         }
     }
     void PickUp()
     {
-        //PLAYER MUST HAVE bool isHolding
-        //OBJECTS MUST HAVE TAG carriable
-        //temporal variables
         int sphereRadius = 5;
         Debug.Log("Trying to Pick Up");
-        if (isHolding)
-        {
-            Debug.Log("Im gonna drop it");
-            DropIt();
-        }
+        //if (isHolding)
+        //{
+        //    Debug.Log("Im gonna drop it");
+        //    DropIt();
+        //}
         Collider[] nearObjects = Physics.OverlapSphere(transform.position, sphereRadius);
         foreach (Collider obj in nearObjects)
         {
-            if (obj.gameObject.tag == "Interactable" && obj.gameObject != carryingObject
-                /*&& Vector3.Dot(obj.gameObject.transform.position,transform.position)/
-                * (obj.gameObject.transform.position.magnitude * transform.position.magnitude) < .065*/)
+            Vector3 difference = (obj.transform.position - transform.position);
+            if (obj.gameObject.tag == "Pickable" && obj.gameObject != carryingObject)
             {
+                //Generating vector to make the dot product
+                Vector3 frontPoint = transform.position + (transform.forward * 2.0f);
+                Vector3 front = frontPoint - transform.position;
+                //Generated the vector for the dot product
 
-                carryingObject = obj.gameObject;
-                carryingObject.transform.SetParent(transform);
-                carryingObject.GetComponent<Rigidbody>().useGravity = false;
-
-                //we apply the same rotation our main object (Camera) has.
-                carryingObject.transform.localRotation = transform.rotation;
-                isHolding = true;
-                break;
+                if ((Vector3.Dot(difference, front) / front.sqrMagnitude) > .25f && (Vector3.Dot(difference, front) / front.sqrMagnitude) < 2.0f)
+                {
+                    carryingObject = obj.gameObject;
+                    carryingObject.transform.SetParent(transform);
+                    beforeGrabRotation = transform.rotation;
+                    //carryingObject.GetComponent<Rigidbody>().useGravity = false;
+                    //we apply the same rotation our main object (Camera) has.
+                    //carryingObject.transform.localRotation = transform.rotation;
+                    isHolding = true;
+                    break;
+                }
             }
         }
     }
     void DropIt()
     {
-        Debug.Log("Trying to Drop");
-        carryingObject.GetComponent<Rigidbody>().useGravity = true;
-        // we don't have anything to do with our ball field anymore
-        carryingObject = null;
-        //Unparent our ball
-        transform.GetChild(0).parent = null;
-        isHolding = false;
+        if(isHolding)
+        {
+            //Debug.Log("Trying to Drop");
+            //carryingObject.GetComponent<Rigidbody>().useGravity = true;
+            // we don't have anything to do with our ball field anymore
+            carryingObject = null;
+            //Unparent our ball
+            transform.GetChild(0).parent = null;
+            isHolding = false;
+        }
+        
     }
 }
