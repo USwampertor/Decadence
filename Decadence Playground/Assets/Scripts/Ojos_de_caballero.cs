d9b402e9cfbd59227ec53a995f2812e5b840c9a2
jalilraziel <idv16c.jzavala@uartesdigitales.edu.mx>
2018-07-29 20:26:53 -0500

    Efectos teclando q

---
+++ Decadence Playground/Assets/Scripts/Ojos_de_caballero.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Threading;
+
+public class Ojos_de_caballero : MonoBehaviour {
+
+    //public int i;
+    public GameObject[] efectos;
+    int i = 0;
+    // Use this for initialization
+    void Start () {
+
+    }
+    void Unsegundo()
+    {
+        StartCoroutine("Wait");
+    }
+    IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(.65f);
+
+        efectos[i].SetActive(false);
+        i++;
+        if (i == efectos.Length) {
+            i = 0;
+        }
+    }
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            efectos[i].SetActive(true);
+            Unsegundo();
+        }
+    }
+}
+
